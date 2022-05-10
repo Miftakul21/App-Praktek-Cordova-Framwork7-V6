4a2332ef182e0dc9c33618e48ec35772739ded8c
Miftakul21 <miftakulhudhud@gmail.com>
2022-05-10 19:36:53 +0700

    first commit

---
+++ www/js/routes.js
@@ -0,0 +1,22 @@
+var routes = [
+  { path: '/', componentUrl: 'halaman/home.html' },
+  {
+    path: '/film/', componentUrl: 'halaman/film.html',
+    options: {
+      transition: 'f7-circle',
+    },
+  },
+  {
+    path: '/dfilm/', componentUrl: 'halaman/detail_film.html',
+    options: {
+      transition: 'f7-circle',
+    },
+  },
+  {
+    path: '/mod/', componentUrl: 'halaman/modul.html',
+    options: {
+      transition: 'f7-circle',
+    },
+  },
+  { path: '(.*)', Url: 'pages/404.html' },
+]
