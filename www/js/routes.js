795c643f8a5df4715591da5f2b3c698c474c38f6
Miftakul21 <miftakulhudhud@gmail.com>
2022-05-11 13:58:54 +0700

    Modal sheet form non accepted

--- www/js/routes.js
+++ www/js/routes.js
@@ -18,5 +18,17 @@ var routes = [
       transition: 'f7-circle',
     },
   },
+  {
+    path: '/dlokal/', componentUrl: 'halaman/datalokal.html',
+    options: {
+      transition: 'f7-circle',
+    },
+  },
+  {
+    path: '/addlokal/', componentUrl: 'halaman/tambahdata.html',
+    options: {
+      transition: 'f7-circle',
+    },
+  },
   { path: '(.*)', Url: 'pages/404.html' },
 ]
